a74f36e7e7d1cf9bbf66aad3e96ff9716de59601
MunkhtengerDev <158283849+MunkhtengerDev@users.noreply.github.com>
2024-02-03 11:34:41 +0800

    Update index.js

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const api = axios.create({ baseURL: 'https://mybook-backend.vercel.app' });
+const api = axios.create({ baseURL: 'https://mybook-backend.vercel.app/' });
 
 api.interceptors.request.use((req) => {
     if (localStorage.getItem('auth')) {
